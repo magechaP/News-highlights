26a35ed90051272f193203d346d648d44af784f6
magechaP <magechapeter@gmail.com>
2019-05-22 11:45:00 +0300

    configru general parent class

---
+++ config.py
@@ -0,0 +1,15 @@
+import os
+
+class Config:
+    '''
+    General configuration parent class
+    '''
+
+    SOURCES_BASE_URL='https://newsapi.org/v2/sources?language=en&category={}&apiKey={}'
+    EVERYTHING_BASE_URL='https://newsapi.org/v2/everything?sources={}&apiKey={}'
+    SEARCH_API_BASE_URL = 'https://newsapi.org/v2/everything?language=en&q={}&apiKey={}'
+    NEWS_API_KEY = os.environ.get('NEWS_API_KEY')
+    SECRET_KEY = os.environ.get('SECRET_KEY')
+
+
+    
