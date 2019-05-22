55f6fa1c9724cc451b216e5b2e76fb6b72cb7661
magechaP <magechapeter@gmail.com>
2019-05-22 12:05:32 +0300

    import

--- app/requests.py
+++ app/requests.py
@@ -10,3 +10,29 @@ sources_url = None
 everything_url = None
 #Search url
 search_url = None
+
+def configure_request(app):
+    global api_key,everything_url,search_url,sources_url
+    api_key = app.config['NEWS_API_KEY']
+    sources_url = app.config['SOURCES_BASE_URL']
+    everything_url=app.config['EVERYTHING_BASE_URL']
+    search_url=app.config["SEARCH_API_BASE_URL"]
+
+def get_newsource(category):
+    '''
+    Function that gets the json response to our url request
+    '''
+    get_newsource_url = sources_url.format(category,api_key)
+
+    with urllib.request.urlopen(get_newsource_url) as url:
+        get_newsource_data = url.read()
+        get_newsource_response = json.loads(get_newsource_data)
+
+
+        newsource_results = None
+
+        if get_newsource_response['sources']:
+            newsource_results_list = get_newsource_response['sources']
+            newsource_results = process_results(newsource_results_list)
+
+    return newsource_results    
