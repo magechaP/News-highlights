a0468440adf83706dacc491c39308e7e40f48910
magechaP <magechapeter@gmail.com>
2019-05-22 11:42:59 +0300

    finalize instance

--- manage.py
+++ manage.py
@@ -4,3 +4,8 @@ from flask_script import Manager,Server
 #Creating app instance
 app = create_app('development')
 
+manager = Manager(app)
+manager.add_command('server',Server)
+
+if __name__ == '__main__':
+    manager.run()
