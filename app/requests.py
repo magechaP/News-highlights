989b8a50ed3f3d0fed63a44b86039c74fe0fc3ef
magechaP <magechapeter@gmail.com>
2019-05-22 12:13:13 +0300

    define a method for processing news articles

--- app/requests.py
+++ app/requests.py
@@ -75,4 +75,27 @@ def get_articles(source_id):
             articles_results_list = get_articles_response['articles']
             articles_results = process_articles(articles_results_list)
 
-    return articles_results    
+    return articles_results
+
+def process_articles(articles_list):
+    '''
+    Function  that processes the new articles and transform them to a list of Objects
+    Args:
+        articles_list: A list of dictionaries that contain article details
+    Returns :
+        articles_results: A list of article objects
+    '''
+    articles_results = []
+    for article_item in articles_list:
+     
+        title = article_item.get('title')
+        description = article_item.get('description')
+        url = article_item.get('url')
+        urlToImage = article_item.get('urlToImage')
+        
+
+        if urlToImage:
+            articles_object = Article(title, description, url, urlToImage)
+            articles_results.append(articles_object)
+
+    return articles_results
