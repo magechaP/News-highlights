f2cebb25457428ac2986d83fad84388c90ec81cf
magechaP <magechapeter@gmail.com>
2019-05-22 12:15:20 +0300

    create a class to define source objects

---
+++ app/models.py
@@ -0,0 +1,11 @@
+class Source:
+    '''
+    Source class to define Source Objects
+    '''
+
+    def __init__(self,id,name,title,description,url):
+        self.id =id
+        self.name=name
+        self.title = title
+        self.description = description
+        self.url = url
